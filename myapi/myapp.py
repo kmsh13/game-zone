4f76aa2ebcacb8b31a7169d0c603908dad42d647
Kamesh J <kamesh.jungi@sungardas.com>
2019-02-17 17:10:48 +0000

    Add api changes

--- myapi/myapp.py
+++ myapi/myapp.py
@@ -12,14 +12,14 @@ def index():
    with sqlite3.connect("database.db") as con:
        cur = con.cursor()
        cur.execute("select count(rank) from gamedata;")
-       count = cur.fetchone();
-       cur.execute("select count(Rank) cnt, Publisher from gamedata group by Publisher Order by cnt desc")
+       count = cur.fetchall();
+       cur.execute("select count(Rank) cnt, Publisher from gamedata group by Publisher Order by cnt desc limit 5")
        publisher = cur.fetchall();
-       cur.execute("select count(Rank) cnt, Genre from gamedata group by Genre Order by cnt desc")
+       cur.execute("select count(Rank) cnt, Genre from gamedata group by Genre Order by cnt desc  limit 5")
        genre = cur.fetchall();
-       cur.execute("select count(Rank) cnt, Year from gamedata group by Year Order by cnt desc")
+       cur.execute("select count(Rank) cnt, Year from gamedata group by Year Order by cnt desc  limit 5")
        year = cur.fetchall();
-       data = {"count":count,"publisher":publisher,"genre":genre,"year":year}
+       data = {"total":count,"publisher":publisher,"genre":genre,"year":year}
        return json.dumps(data)
    
 

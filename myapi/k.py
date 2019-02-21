01915220bcfc7923d2e23e58adddc36c07ab0136
Kamesh Jungi <kamesh.jungi@sungardas.com>
2019-02-21 20:06:30 +0530

    Update file and code

--- myapi/k.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-
-with sqlite3.connect("database.db") as con:
-    cur = con.cursor()
-    cur.execute("select count(rank) from gamedata;")
-    count = cur.fetchone();
-    print count
